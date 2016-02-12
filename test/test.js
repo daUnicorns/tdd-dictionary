f6a6ee1bb8020a4e1cb5244b94d81a29119f19b7
virginie <virginietrubiano@gmail.com>
2016-02-12 10:17:02 +0000

    removed the qUnit testing framework

--- test/test.js
+++ test/test.js
@@ -4,21 +4,8 @@ var test = require('tape');
 var main = require('../main.js');
 var apiKey = require("../apiKey.js"), api = apiKey.apiKey;
 
-// test("1. Trying to return the searched word from the wordnik api", function (t) {
-//     t.deepEqual(main.getAPIobject(api, "cat"), "cat", "success!");
-//     t.end();
-// });
 
-QUnit.test( "hello test", function( assert ) {
-  assert.ok( 1 == "1", "Passed!" );
-
-  assert.equal(typeof main, 'object');
-  console.log("BOUH");
-  assert.equal(main.getAPIobject(api, "cat"), "cat");
-}
-
-
-
-QUnit.test( "hello test", function( assert ) {
-  assert.ok( 1 == "1", "Passed!" );
+test("1. Trying to return the searched word from the wordnik api", function (t) {
+    t.deepEqual(main.getAPIobject(api, "cat"), "cat", "success!");
+    t.end();
 });
