b4194b069a54ff1adea2ebee14a6d6bcd359ece5
RobStallion <Robertefrancis18@gmail.com>
2016-02-12 12:20:22 +0000

    solved the merge conflicts and it looks all good

--- test/robtest.js
+++ test/robtest.js
@@ -1,19 +1,27 @@
 var test = require('tape');
 var main2 = require('../main2.js');
 var colors = require('colors');
-var main = require('../main.js')
-
-test("tests if main.js has access to words.txt", function(t){
-   t.deepEquals(main2.wordImporter('A'), 0 , "I can't believe it! A is position 0 in the array");
+var main = require('../main.js');
+test("tests if main.js has access to words.txt", function(t) {
+   t.deepEquals(main2.wordImporter('A'), 0, "I can't believe it! A is position 0 in the array");
    t.end();
 });
-
-test("tests if main.js will return an array including the searched word and the 4 that come after it", function(t){
+test("tests if main.js will return an array including the searched word and the 4 that come after it", function(t) {
    t.deepEquals(main2.arrayMaker("A"), ['A', 'a', 'aa', 'aal', 'aalii'], "There's an array baby!!");
    t.end();
 });
-
-test("tests if I can grab the API key", function(t){
+test("tests if I can grab the API key", function(t) {
    t.deepEquals(main2.printApi().length, 49, "we hid the api key, such magic so magician ");
    t.end();
+});
+test("does the getAPIobject function return the word that we searched", function(t) {
+   var result;
+   var expected = "A set of written, printed, or blank pages fastened along one side and encased between protective covers.";
+   main.getDefinition(process.env.DB_API, "book", function(word) {
+      result = word;
+   });
+   setTimeout(function(x) {
+      t.deepEquals(result, expected, "success!!!!!!!!");
+      t.end();
+   }, 1000);
 });
--- test/test.js
+++ test/test.js
@@ -1,19 +1,19 @@
-// var assert = require('assert');
-// var testindex = 1; // see: https://testanything.org/tap-version-13-specification.html
-// var test = require('tape');
-var main = require('../main.js');
-
-// var apiKey = require("../apiKey.js"), api = apiKey.apiKey;
+// // var assert = require('assert');
+// // var testindex = 1; // see: https://testanything.org/tap-version-13-specification.html
+// // var test = require('tape');
+// var main = require('../main.js');
 //
-// // test("1. Trying to return the searched word from the wordnik api", function (t) {
-// //     t.deepEqual(main.getAPIobject(api, "cat"), "cat", "success!");
-// //     t.end();
-// // });
-
-test("bsdfbfgbgndnh", function(assert){
-  assert.equal(typeof main, 'object');
-});
-
-test ("asdfasdfasdf", function(assert){
-  assert.equal(typeof main.import, 'function');
-});
+// // var apiKey = require("../apiKey.js"), api = apiKey.apiKey;
+// //
+// // // test("1. Trying to return the searched word from the wordnik api", function (t) {
+// // //     t.deepEqual(main.getAPIobject(api, "cat"), "cat", "success!");
+// // //     t.end();
+// // // });
+//
+// test("bsdfbfgbgndnh", function(assert){
+//   assert.equal(typeof main, 'object');
+// });
+//
+// test ("asdfasdfasdf", function(assert){
+//   assert.equal(typeof main.import, 'function');
+// });
