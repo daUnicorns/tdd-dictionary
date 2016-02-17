b43b4c3b1064fe3d15baa93b14059d4c7ef10157
RobStallion <Robertefrancis18@gmail.com>
2016-02-17 12:05:59 +0000

    added for loop to make words empty again

--- main.js
+++ main.js
@@ -1,48 +1,56 @@
 function importData() {
-  var xhr = new XMLHttpRequest();
-  var formData = document.getElementById("input").value;
-  xhr.onreadystatechange = function() {
-    if(xhr.readyState == 4 && xhr.status == 200) {
-      var res = xhr.responseText.split("*");
-      printData(res);
-    }
-  };
+   var xhr = new XMLHttpRequest();
+   var formData = document.getElementById("input").value;
+   xhr.onreadystatechange = function() {
+      if (xhr.readyState == 4 && xhr.status == 200) {
+         var res = xhr.responseText.split("*");
+         printData(res);
+      }
+   };
 
-  setTimeout(function() {
-    xhr.open("POST", "/search" + formData);
-    xhr.send();
-  }, 1000);
+   setTimeout(function() {
+      xhr.open("POST", "/search" + formData);
+      xhr.send();
+   }, 1000);
 }
 
 function printData(arr) {
-  var rightAnswer = function() {
-    for(var i=0; i<arr.length; i++) {
-      if(arr[i].indexOf("$") > -1) return arr[i].replace("$", "");
-    }
-};
+   var rightAnswer = function() {
+      for (var i = 0; i < arr.length; i++) {
+         if (arr[i].indexOf("$") > -1) return arr[i].replace("$", "");
+      }
+   };
 
-  var classElements = document.getElementsByClassName("spaceship");
-  for(var i=0; i < (arr.length) -1; i++) {
-    if(arr[i].indexOf("$") > -1) {
-      classElements[i].innerHTML = arr[i].replace("$", "");
-      classElements[i].setAttribute("id", "right-answer");
-    } else {
-      classElements[i].innerHTML = arr[i];
-      classElements[i].setAttribute("id", "wrong-answer");
-    }
-  }
+   var classElements = document.getElementsByClassName("spaceship");
+   for (var j = 0; j < 5; j++) {
+      classElements[j].innerHTML = '';
+      classElements[j].setAttribute("id", "wrong-answer");
+   }
+   for (var i = 0; i < (arr.length) - 1; i++) {
+      if (arr[i] === undefined) {
+         classElements[i].innerHTML = "";
+         classElements[i].setAttribute("id", "wrong-answer");
+      }
+      if (arr[i].indexOf("$") > -1) {
+         classElements[i].innerHTML = arr[i].replace("$", "");
+         classElements[i].setAttribute("id", "right-answer");
+      } else {
+         classElements[i].innerHTML = arr[i];
+         classElements[i].setAttribute("id", "wrong-answer");
+      }
+   }
 
 
-  var definition = arr[arr.length-1];
-  var defElement = document.getElementById('definition');
-  defElement.innerHTML = definition;
-  defElement.setAttribute("class", "background");
-  document.getElementById('definitionBox').setAttribute("class", "flyingdefinition flyin");
+   var definition = arr[arr.length - 1];
+   var defElement = document.getElementById('definition');
+   defElement.innerHTML = definition;
+   defElement.setAttribute("class", "background");
+   document.getElementById('definitionBox').setAttribute("class", "flyingdefinition flyin");
 }
 
 document.getElementById("form").addEventListener("submit", function(e) {
-  e.preventDefault();
-  importData();
+   e.preventDefault();
+   importData();
 });
 
 module.exports = {
